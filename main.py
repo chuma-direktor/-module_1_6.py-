ec31fe0941a9d967c4d0847c0f2b0df10194bdec
chuma-direktor <altaichuma04@gmail.com>
2024-10-08 14:42:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+my_dict = {'dad' : 1985, 'mom' : 1984, 'brother' : 2009, 'sister' : 2011}
+print(my_dict)
+print(my_dict['dad'])
+my_dict ['grandmother'] = 1962
+my_dict ['grandfather'] = 1959
+print(my_dict)
+del my_dict ['brother']
+print(my_dict)
+
+my_set = {1,2,3,4,5,1,2,3,4, 'Горно-Алтайск', 'Бийск',True, (1,2,3)}
+print(my_set)
+print(my_set.discard(1))
+print(my_set.add(7))
+
